923bc569bdca2bb151677da58dc9f3994239d9fc
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-20 21:57:33 +0200

    Countries added in a different folder structure

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    // ** data amader ashtew pare nau pare
+    const [countries,setCountries] = useState([]);
+
+    // ** data load 
+    useEffect(()=>{
+        // ** data loader function async
+        const countriesData = async ()=>{
+            try {
+                const response = await fetch(`https://restcountries.com/v3.1/all`);
+                response.ok ? console.log('Successfully loaded') : console.log('failed');
+                const data = await response.json();
+                setCountries(data)
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        countriesData();
+    },[])
     return (
         <div>
-            <h1>My countries will be displayed below.</h1>
+            <h1>Countries Avaiable:{countries.length}</h1>
+            {
+                countries?.map(country=> <p>Name: {country?.name?.common} </p> )
+            }
         </div>
     );
 };
 
-export default Countries;<h1>My countries will be displayed below.</h1>
+export default Countries;
