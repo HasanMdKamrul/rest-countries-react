c727766240832b24248cbad942ac3a54fd258143
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-20 20:13:22 +0200

    Data successfully loaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
@@ -13,17 +13,26 @@ function App() {
 // ** First make the component
 
 function Countries(){
+  // ** 2 things can happen we can get the data or not, so obostar change hosse we need state
+  const [countries,setCountries] = useState([]);
   // ** Load the data from the api
   useEffect(()=>{
     // ** Loader function
     const restCountries = async ()=>{
       try {
-        
+        const response = await fetch(`https://restcountries.com/v3.1/all`);
+        response.ok ? console.log('Successful') : console.log('failed');
+        const data = await response.json();
+        setCountries(data)
       } catch (error) {
         console.log(error);
       }
     }
-  },[])
+    restCountries();
+  },[]);
+
+  
+
   return(
     <div>
       <h1>Rest Countries .....</h1>
